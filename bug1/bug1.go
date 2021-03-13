45d160d1771db5f61d9cae894879f24f5d0920ac
Seunghyup Alex Oh <seunghyupoh3517@gmail.com>
2021-03-12 20:09:25 -0800

    Done

--- bug1/bug1.go
+++ bug1/bug1.go
@@ -1,11 +1,16 @@
 package bug1
+import "sync" 
+// Using mutex to synchronize
 
 // Counter stores a count.
 type Counter struct {
 	n int64
+	mu sync.Mutex
 }
 
 // Inc increments the count in the Counter.
 func (c *Counter) Inc() {
+	c.mu.Lock()
 	c.n++
+	c.mu.Unlock()
 }
